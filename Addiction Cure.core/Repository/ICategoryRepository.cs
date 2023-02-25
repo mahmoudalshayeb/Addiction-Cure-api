c54433b02e082b85ec81a6849a6e38244374521c
Yaman1999 <awawdehyaman51@gmail.com>
2023-02-25 19:09:47 +0300

    yaman

---
+++ Addiction Cure.core/Repository/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Addiction_Cure.core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addiction_Cure.core.Repository
+{
+    public interface ICategoryRepository
+    {
+
+        List<Categoryac> GetAllCategoryAC();  
+        
+        Categoryac GetCategoryByIdAC(int id);
+
+        void CreateCategoryAC(Categoryac categoryac);
+        void UpdateCategoryAC(Categoryac categoryac);
+
+        void DeleteCategoryAC(int id);
+
+
+    }
+}
---
+++ Addiction Cure.core/Repository/ITestimonialsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Addiction_Cure.core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addiction_Cure.core.Repository
+{
+    public interface ITestimonialsRepository
+    {
+
+        List<Testemonialac> GetAllTestimonialAC();
+
+        Testemonialac GetTestimonialByIdAC(int id);
+
+        void CreateTestimonialAC(Testemonialac testemonialac);
+        void UpdateTestimonialAC(Testemonialac testemonialac);
+
+        void DeleteTestimonialAC(int id);
+    }
+}
