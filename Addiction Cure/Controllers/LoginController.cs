d585f206187690891fd22a3f7cad4be89d6d2de9
aboodghx <abedabood009@gmail.com>
2023-03-17 01:22:53 +0300

    assd

--- Addiction Cure/Controllers/LoginController.cs
+++ Addiction Cure/Controllers/LoginController.cs
@@ -4,6 +4,9 @@ using Addiction_Cure.core.Service;
 using Addiction_Cure.infra.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+using SixLabors.ImageSharp;
 
 namespace Addiction_Cure.Controllers
 {
@@ -34,6 +37,26 @@ namespace Addiction_Cure.Controllers
              loginService.register(patient);
         }
 
+        [HttpPost]
+        [Route("uploadImage")]
+        public Register UploadIMage()
+        {
+            var file = Request.Form.Files[0]; // 0 means the first image in postman  FORM DATA
+            var fileName = Guid.NewGuid().ToString() + "_" + file.FileName; // INCYPTION OF THE IMAGE
+            var fullPath = Path.Combine("C:\\Users\\User\\Addiction-Cure-Angular\\src\\assets\\images", fileName); // GET THE IMAGE AND ADD IT TO IMAGES FILE IN OUR PROJECT
+
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            // its like insert TEMPORARY record  from category all OF ITS  ATTRIBUTS  are null except the image //// NOT IN DATABASE ITS JUST TEMPORRARY
+
+            Register item = new Register();
+            item.Imagename = fileName;
+            return item;
+        }
 
 
         [HttpPost]
