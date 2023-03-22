1611c0ce9f35231fda38dadac5d60731ce4cd8f6
mahmoud <1750901046@st.aabu.edu.jo>
2023-03-22 22:32:23 +0300

    dd

--- Addiction Cure.infra/Repository/TestRepository.cs
+++ Addiction Cure.infra/Repository/TestRepository.cs
@@ -28,6 +28,7 @@ namespace Addiction_Cure.infra.Repository
         {
             var p = new DynamicParameters();
             p.Add("quationIDac", test.Quastionid, dbType: DbType.Int64, ParameterDirection.Input);
+            p.Add("testdateac", test.Testdate, dbType: DbType.Date, ParameterDirection.Input);
             p.Add("statusac", test.Status, dbType: DbType.Int32, ParameterDirection.Input);
             p.Add("paitenidac", test.Patientid, dbType: DbType.Int32, ParameterDirection.Input);
             dbContext.Connection.Execute("Testac_pack.CreateTestac",p,commandType:CommandType.StoredProcedure);
